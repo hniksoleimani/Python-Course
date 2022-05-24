66ee63ee2db530c38776a3318e45521dafeeaef5
Hani <94698402+hniksoleimani@users.noreply.github.com>
2022-05-24 21:06:13 +0430

    Update Ex2.py

--- Python-Course/Assignment04/Ex2.py
+++ Python-Course/Assignment04/Ex2.py
@@ -1,11 +1,18 @@
 from math import sqrt
-
+# def delta(a,b,c):
 def EQN(a,b,c):
-    delta = (b*2)-(4*a*c)
-    ans1 = (-b+sqrt(delta))/2*a 
-    ans2 = (-b-sqrt(delta))/2*a
-    print(ans1)
-    print(ans2)
+    delta = (b**2)-(4*a*c)
+    if delta <0: 
+        print('No Answer')
+    elif delta == 0:
+        x = -b/a
+        print('1 Answer:', x)
+
+    elif delta > 0:
+        x1 = (-b+sqrt(delta))/2*a 
+        x2 = (-b-sqrt(delta))/2*a
+        print('2 Answers', x1, x2)
+
 
 
 m = int(input('Enter a:'))
