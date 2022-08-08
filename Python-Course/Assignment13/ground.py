d197140e2fbc87125a3d1bc3eec1179584b6a47f
Hani <94698402+hniksoleimani@users.noreply.github.com>
2022-08-08 10:47:05 +0430

    Add files via upload

---
+++ Python-Course/Assignment13/ground.py
@@ -0,0 +1,12 @@
+import arcade
+class Ground(arcade.Sprite):
+    def __init__(self, x , y):
+        super().__init__()
+        
+        self.texture = arcade.load_texture(":resources:images/tiles/grassMid.png")
+        self.center_y = y
+        self.center_x = x
+        
+        self.width = 120
+        self.height = 120
+
